4983da9a2083635923a054856d690197ae6e7ca8
Hugo Benitez <hugo.boca16@gmail.com>
2026-09-22 18:18:12 -0300

    OpenApi auth-service

--- auth-service/src/main/java/com/auth/service/auth_service/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/auth/service/auth_service/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.auth.service.auth_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,19 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                // Esto aplica el requerimiento de seguridad de forma global (opcional,
+                // si prefieres agregarlo endpoint por endpoint puedes omitir esta línea)
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Auth - Service")
                         .version("1.0.0")
