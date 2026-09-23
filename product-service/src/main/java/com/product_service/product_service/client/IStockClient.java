ea5829bc6c91f56f7cd0b55fed170805ab454656
Hugo Benitez <hugo.boca16@gmail.com>
2026-09-23 17:40:24 -0300

    dockerfile product-service

--- product-service/src/main/java/com/product_service/product_service/client/IStockClient.java
+++ product-service/src/main/java/com/product_service/product_service/client/IStockClient.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
-//@FeignClient(name = "stock-service")
+
 @FeignClient(name = "stock-service", fallbackFactory = StockClientFallbackFactory.class)
 public interface IStockClient {
 
